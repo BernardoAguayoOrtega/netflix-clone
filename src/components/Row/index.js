8a264d7965755030850a8527e077d951222d9bb6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-11 12:05:56 -0500

    connection succefuly with tmdb

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,14 +1,28 @@
 //import React and its hooks
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+//import instance const
+import { instance } from '../../utils/request';
 
 //create and export Row component
 export const Row = ({ title, fetchUrl }) => {
-  //use state hook
-  const [movies, SetMovies] = useState([])
+	//use state hook
+	const [movies, SetMovies] = useState([]);
 
-  return(
-    <>
-      <h1>{title}</h1>
-    </>
-  )
-}
+	//use use effect hook
+	useEffect(() => {
+		const fetchData = async () => {
+      const request = await instance(fetchUrl)
+      .then(response => response.json())
+			console.dir(request);
+    };
+    
+    fetchData()
+
+	});
+
+	return (
+		<>
+			<h1>{title}</h1>
+		</>
+	);
+};
