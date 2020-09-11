b641544f2a2f11ce2ec17935f47c73c14afa249a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-11 09:15:33 -0500

    set up the dir structure

---
+++ src/components/App/index.js
@@ -0,0 +1,9 @@
+//import react
+import React from 'react'
+
+//create and export app component
+export const App = () => {
+  return(
+    <h1>I'm the app component</h1>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+//import react
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import render method
+import { render } from 'react-dom';
+//import app component
+import { App } from './components/App';
+//import serviceWorker
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//root const
+const root = document.getElementById('root');
+
+render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	root,
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//call the serviceWorker
+serviceWorker.register();
