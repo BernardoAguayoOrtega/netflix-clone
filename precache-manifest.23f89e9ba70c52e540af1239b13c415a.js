b43e44902fd133fddcf654d90049ad700835a814
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-14 13:38:27 -0500

    Updates

---
+++ precache-manifest.23f89e9ba70c52e540af1239b13c415a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715b1bc447574621a9eedc75368f6412",
+    "url": "/(https://bernardoaguayoortega.github.io/netflix-clone/index.html"
+  },
+  {
+    "revision": "3ba42954bb7522cc006a",
+    "url": "/(https://bernardoaguayoortega.github.io/netflix-clone/static/css/main.3221ec0e.chunk.css"
+  },
+  {
+    "revision": "5080f67891a10433d229",
+    "url": "/(https://bernardoaguayoortega.github.io/netflix-clone/static/js/2.8622fe79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/(https://bernardoaguayoortega.github.io/netflix-clone/static/js/2.8622fe79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba42954bb7522cc006a",
+    "url": "/(https://bernardoaguayoortega.github.io/netflix-clone/static/js/main.23cfd26d.chunk.js"
+  },
+  {
+    "revision": "6da7a00aac1f16f0c525",
+    "url": "/(https://bernardoaguayoortega.github.io/netflix-clone/static/js/runtime-main.4d3ee10a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/(https://bernardoaguayoortega.github.io/netflix-clone/precache-manifest.23f89e9ba70c52e540af1239b13c415a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/(https://bernardoaguayoortega.github.io/netflix-clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
