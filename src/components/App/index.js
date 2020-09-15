ec78fbec2ee893a0422144b12d5e6d8d9e6e7905
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-15 08:59:36 -0500

    set up the nav component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Banner } from '../Banner';
 //import list of rows component
 import { ListOfRows } from '../ListOfRows';
+//import nav component
+import { Nav } from '../Nav';
 
 //create and export app component
 export const App = () => {
 	return (
 		<>
+			<Nav />
 			<Banner />
 			<ListOfRows />;
 		</>
---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+//import react
+import React from 'react';
+
+//create and export Nav functional component
+export const Nav = () => {
+	return (
+		<>
+			<h1>I'm the</h1>
+			<p>Nav bar</p>
+		</>
+	);
+};
